54eeec67272d8f507bd3a8bad78d028bb802f92b
“Artyom <arkrasovski1996@gmail.com>
2022-09-13 09:59:37 +0300

    feat: solve the task

--- PalindromicNumber/NumbersExtension.cs
+++ PalindromicNumber/NumbersExtension.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace PalindromicNumberTask
 {
@@ -15,7 +16,39 @@ namespace PalindromicNumberTask
         /// <exception cref="ArgumentException"> Thrown when source number is less than zero. </exception>
         public static bool IsPalindromicNumber(int number)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (number < 0)
+            {
+              throw new ArgumentException("number cannot be less than zero");
+            }
+
+            int count = 0;
+            int numberCopy = number;
+            int numberCopy2 = number;
+            while (numberCopy > 0)
+            {
+                numberCopy /= 10;
+                count++;
+            }
+
+            int min = number % 10;
+            for (int i = 0; i < count - 1; i++)
+            {
+                numberCopy2 /= 10;
+            }
+
+            if (count == 0 || count == 1)
+            {
+                return true;
+            }
+
+            int max = numberCopy2;
+
+            if (max == min)
+            {
+                return IsPalindromicNumber((int)(((number % Math.Pow(10, count - 1)) - (number % 10)) / 10));
+            }
+
+            return false;
         }
     }
 }
